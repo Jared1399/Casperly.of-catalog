7077a54816617cdb78dd06234f3bb0d6198553af
jared.quiero <jared.quiero@opencasa.com>
2023-10-15 23:27:44 -0300

    added main components and basic js

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const btnToggleTransferData = document.getElementById('toggleTransferData') 
+const transferData = document.getElementById('infoContainer')
+
+btnToggleTransferData.addEventListener('click', () => {
+    transferData.classList.toggle('invisible')
+})
